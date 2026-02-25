3b11d56abd330a324721cde14d2235334d831c7c
Syed Arif Iqbal <arifiqbal@outlook.com>
2026-02-25 17:50:23 +0500

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,6 +37,7 @@ class User extends Authenticatable implements MustVerifyEmail
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
             'is_active' => 'boolean',
+            'tenant_id' => 'integer',
         ];
     }
 
